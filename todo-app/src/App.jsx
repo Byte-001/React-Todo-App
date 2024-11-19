fae9acf80a196a7f8bc1f5d99cff3da52e8ca3b3
ardakoca <ardakoca957@gmail.com>
2024-11-19 21:07:38 +0300

    feat: implement task features with add and toggle functionality

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,13 +1,40 @@
-import Header from './components/Header';
-import Tasks from './components/Tasks';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskTitle) => {
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        completed: false,
+      },
+    ]);
+  };
+
+  const toggleComplete = (taskID) => {
+     const newTasks = tasks.map((task) => {
+         if (task.id === taskID) {
+            return {
+               ...task,
+               completed: !task.completed,
+            };
+         }
+         return task;
+     });
+     setTasks(newTasks);
+  }
+
   return (
     <>
-      <Header/>
-      <Tasks/>
+      <Header onAddTask={addTask} />
+      <Tasks tasks={tasks} onComplete={toggleComplete}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
