a95f0a5b4e9ff9f44b933d0911f0ff41eb048334
ardakoca <ardakoca957@gmail.com>
2024-11-19 21:12:15 +0300

    feat: add task input and submission functionality

--- todo-app/src/components/Header.jsx
+++ todo-app/src/components/Header.jsx
@@ -1,21 +1,46 @@
-import  "../App.css";
-import logo from "../assets/Logo.svg"
+import "../App.css";
+import logo from "../assests/logo.svg";
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import { useState } from "react";
 
+function Header({ onAddTask }) {
+  const [text, setText] = useState("");
 
-function Header() {
-    return (
-        <>
-            <div className="header">
-                <img src={logo} className={"logo"} alt="logo" />
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
 
-                <form className="header__form">
-                    <input className={"todo__input"} placeholder={"Add a new task..."} type={"text"}/>
-                    <button className={"input__button"}>Create<AiOutlinePlusCircle size={26} />
-                    </button>
-                </form>
-            </div>
-        </>
-    )
+  const onChangeHandler = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if(text !== ""){
+       onAddTask(text);
+       setText("");
+    }
+};
+
+  return (
+    <>
+      <div className="header">
+        <img src={logo} className={"logo"} alt="logo" />
+
+        <form className="header__form" onSubmit={onSubmitHandler}>
+          <input
+            className={"todo__input"}
+            placeholder={"Add a new task..."}
+            type={"text"}
+            value={text}
+            onChange={onChangeHandler}
+          />
+          <button className={"input__button"} onClick={handleAddTask}>
+            Create
+            <AiOutlinePlusCircle size={26} />
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 export default Header;
