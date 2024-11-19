b7d98a4afd008450d4e357101e8c9fbce31ec7f5
ardakoca <ardakoca957@gmail.com>
2024-11-19 21:18:35 +0300

    feat: add task completion functionality

--- todo-app/src/components/Task.jsx
+++ todo-app/src/components/Task.jsx
@@ -1,18 +1,19 @@
 import "../App.css";
 import { TbTrash } from "react-icons/tb";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
-function Task(){
-    return (
-       <div className={"task"}>
-           <button className={"check__container"}>
-               <div />
-           </button>
+function Task({ task, onComplete }) { 
+return (
+    <div className={"task"}>
+      <button className={"check__container"} onClick={() => onComplete(task.id)}>
+        {task.completed ?  <BsFillCheckCircleFill size={25} style={{color:"green"}}/> : <div/>}
+      </button>
 
-           <p className={"task__text"}></p>
-           <button className={"delete__button"}>
-               <TbTrash />
-           </button>
-       </div>
-   )
+      <p className={`${task.completed ? "line__decoration task__text" : "task__text"}`}>{task.title}</p>
+      <button className={"delete__button"}>
+        <TbTrash />
+      </button>
+    </div>
+  );
 }
 export default Task;
