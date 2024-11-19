1361f61e81e1993c48ccac87b45d8225ff31c729
ardakoca <ardakoca957@gmail.com>
2024-11-19 22:19:01 +0300

    feat: improve Tasks component for display task count and map through tasks

--- todo-app/src/components/Tasks.jsx
+++ todo-app/src/components/Tasks.jsx
@@ -1,18 +1,32 @@
 import "../App.css";
 import Task from "./Task.jsx";
 
-function Tasks(){
-    return (
-        <div className={"tasks__container"}>
-            <div className={"tasks__header"}>
-                <h3 className={"created__title"}>Created Tasks <span className={"span__number"}>5</span></h3>
-                <h3 className={"completed__title"}>Completed <span className={"span__number"}>2 of 5</span></h3>
-            </div>
-            <div className={"task__list"}>
-                <Task/>
-            </div>
-        </div>
-
-    )
+function Tasks({ tasks, onComplete}) {
+  const taskQuantity = Object.values(tasks).length; 
+  const completedTasks = Object.values(tasks).filter(task => task.completed).length;  
+  
+  return (
+    <div className={"tasks__container"}>
+      <div className={"tasks__header"}>
+        <h3 className={"created__title"}>
+          Created Tasks <span className={"span__number"}>{ taskQuantity }</span>
+        </h3>
+        <h3 className={"completed__title"}>
+          Completed <span className={"span__number"}>{completedTasks} of {taskQuantity}</span>
+        </h3>
+      </div>
+      <div className={"task__list"}>
+        {tasks.map((task) => {
+          return (
+            <Task
+              key={task.id}
+              task={task}
+              onComplete={onComplete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 export default Tasks;
